bde32ab9877fa75edeb68e88f34c9bc28d336873
Qoyun Koyla Avnaena <qoyunkoylaavnaena@gmail.com>
2026-03-05 10:29:45 +0700

    memperbaiki confllict

--- routes/web.php
+++ routes/web.php
@@ -3,31 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
-<<<<<<< HEAD
 use App\Http\Controllers\HeroController;
-
-Route::middleware(['guest'])->group(function () {
-    // route login
-    Route::get('/Login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/Login', [AuthController::class, 'login']);
-=======
 
 Route::get('/', function () {
     return view('index');
 });
 
-
 Route::middleware(['guest'])->group(function () {
-    // route login
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
 
->>>>>>> ad702ceca3af2aadccdfa04aad716fb1a0e63181
-    
+    // route login
+    Route::get('/Login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/Login', [AuthController::class, 'login']);
+
 });
 
 Route::middleware(['auth'])->group(function () {
-    // route admin dasboard
+
+    // route admin dashboard
     Route::get('/Admin', [AdminController::class, 'view'])->name('admin');
 
     // route tabel user
@@ -36,12 +28,12 @@ Route::middleware(['auth'])->group(function () {
     // route logout
     Route::post('/Logout', [AuthController::class, 'logout'])->name('logout');
 
-    // route portofolio
+    // route hero
     Route::get('/Hero', [HeroController::class, 'view'])->name('hero.index');
-    Route::get('/Hero/Create', [HeroController::class, 'create'])->name('hero.create');
-    Route::post('/Hero/Create', [HeroController::class, 'store'])->name('hero.store');
-    Route::get('/Hero/Edit/{hero}', [HeroController::class, 'edit'])->name('hero.edit');
-    Route::put('/Hero/Edit/{hero}', [HeroController::class, 'update'])->name('hero.update');
+    Route::get('/Hero/create', [HeroController::class, 'create'])->name('hero.create');
+    Route::post('/Hero/create', [HeroController::class, 'store'])->name('hero.store');
+    Route::get('/Hero/edit/{hero}', [HeroController::class, 'edit'])->name('hero.edit');
+    Route::put('/Hero/edit/{hero}', [HeroController::class, 'update'])->name('hero.update');
     Route::delete('/Hero/{hero}', [HeroController::class, 'destroy'])->name('hero.destroy');
-});
 
+});
